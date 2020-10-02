7a4a63ca49fb6815c30f42f9eaf7959148bb72a2
osidalsaghir <36995193+osidalsaghir@users.noreply.github.com>
2020-10-02 23:51:29 +0300

    My message

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,7 +3,7 @@ const CountryContraller = require('../app/Controllers/CountryController');
 
 module.exports = (src) => {
  
-
+  
   src.post("/country/add", CountryContraller.insert);
   src.post("/country/update", CountryContraller.update);
   src.post("/country/view", CountryContraller.view);
